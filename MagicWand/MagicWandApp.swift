ca421fe67b505b5995b1e32f95035c09c67eb3ff
grluo <degrluo@gmail.com>
2023-12-21 03:22:40 +0800

    Initial Commit

---
+++ MagicWand/MagicWandApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagicWandApp.swift
+//  MagicWand
+//
+//  Created by grluo on 2023/12/21.
+//
+
+import SwiftUI
+
+@main
+struct MagicWandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
